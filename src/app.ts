0a6f8235fd38e3720041f9bf01074e0643ea41c4
Hafiz <hafizadhyaulhaq16@gmail.com>
2024-05-31 09:26:15 +0700

    add global handler

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json())
+
 app.use(routes)
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Endpoint not found' });
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
