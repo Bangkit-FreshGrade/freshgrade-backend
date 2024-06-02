3875d2a3de3ea003d0f850f41bd5deef1921a0b5
Hafiz <hafizadhyaulhaq16@gmail.com>
2024-06-02 10:11:37 +0700

    feat: get user details

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import * as AuthService from "./auth.service";
+import authVerification from "../../common/guard/at.guard";
 
 const router = Router();
 
@@ -25,6 +26,29 @@ router.post('/signin', async (req: Request, res: Response) => {
   try {
     const tokens = await AuthService.login({ ...req.body});
     res.json(tokens)
+  } catch (error: any) {
+    res.status(error.errorCode).json({
+      code: error.errorCode,
+      message: error.message
+    })
+  }
+})
+
+/**
+ * Get User Details
+ */
+router.get('/user', authVerification, async (req: Request, res: Response) => {
+  const userId = req.params.id
+  if (!userId) {
+    return res.status(401).json({
+      code: 401,
+      message: "Unauthorized"
+    })
+  }
+
+  try {
+    const user = await AuthService.getUserDetails(userId)
+    res.json(user)
   } catch (error: any) {
     res.status(error.errorCode).json({
       code: error.errorCode,
