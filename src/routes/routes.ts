0cc28682683fc47646c87063858579493e16d9a5
hafizhdh <hafizadhyaulhaq16@gmail.com>
2024-06-12 07:01:37 +0700

    implement scan feature

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import authController from "./auth/auth.controller"
 import articleController from "./articles/article.controller"
+import scannerController from "./scanner/scanner.controller"
 
 const api = Router()
   .use(authController)
   .use(articleController)
+  .use(scannerController)
 
 export default Router().use('/api', api)
-Router().use('/articles', articleController);
